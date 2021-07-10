0b45a9c8841d25717611424628e7628073cb2395
Mali <mali.tshabs@gmail.com>
2021-07-10 09:01:49 +0200

    made changes to pom.xml

--- src/main/java/com/hicogroup/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/hicogroup/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,12 +2,20 @@ package com.hicogroup.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestfulWebServicesApplication {
+public class RestfulWebServicesApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
+		System.out.println("App Has Started Successfully");
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(RestfulWebServicesApplication.class);
 	}
 
 }
